f617b4bf65c2ae1e37a11114e8a8ca3992052299
Сандро <warsan00@mail.ru>
2020-09-10 04:51:21 +0500

    Update config.js

--- sample-app/src/config.js
+++ sample-app/src/config.js
@@ -1,6 +1,6 @@
 module.exports.DB_FILE_NAME = './database.txt'
 module.exports.API_BASE = 'https://api.miro.com/v1'
-module.exports.BASE_URL = 'https://-------.ngrok.io' // Must be replaced after each restart ngrok
-module.exports.CLIENT_ID = '1004457346318492837' // Must be replaced from "manage apps" settings
-module.exports.CLIENT_SECRET = '5KSBs8DHnqKMq87iuybci' // Must be replaced from "manage apps" settings
-module.exports.WEBHOOKS_VERIFICATION_TOKEN = '' // Must be replaced from "manage apps" settings. Coming soon
+module.exports.BASE_URL = 'https://-------.ngrok.io' // Необходимо заменять после каждого перезапуска ngrok
+module.exports.CLIENT_ID = '1004457346318492837' // Необходимо заменить в настройках "Управление приложениями"
+module.exports.CLIENT_SECRET = '5KSBs8DHnqKMq87iuybci' // Необходимо заменить в настройках "Управление приложениями"
+module.exports.WEBHOOKS_VERIFICATION_TOKEN = '' // Необходимо заменить в настройках «Управление приложениями». Скоро будет
